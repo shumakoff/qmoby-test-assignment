bfa1898a9d43d299a393d94dd303b382cf69abdb
Anatolii Shumakov <a.shumakoff@protonmail.com>
2021-09-13 20:35:13 +0500

    Packages installed

---
+++ front/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <Content>
+  </Content>
+
+</template>
+
+<script>
+import Content from './components/Content.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'Content': Content,
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  },
+
+  computed: {
+
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ front/src/components/Content.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    State your name
+    <p>
+      <input v-model="playerName">
+    </p>
+    <button v-on:click="login">Let's go</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      playerName: "",
+    }
+  },
+
+  methods: {
+    log(message) {
+      console.log(message);
+    },
+    login() {
+      console.log(this.playerName + " enters game");
+    },
+  },
+  computed: {
+
+    },
+}
+</script>
+
+<style>
+</style>
+
